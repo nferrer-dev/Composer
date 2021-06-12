f049d7096a06c5d63aea023780c0562e0941f8b6
Nigel Ferrer <nferrertech@gmail.com>
2021-06-11 22:30:46 -0400

    incorporating velocity into midi_to_samples

--- midi_utils.py
+++ midi_utils.py
@@ -7,6 +7,21 @@ Utils to read and write midi.
 
 from mido import MidiFile, MidiTrack, Message
 import numpy as np
+# import itertools
+
+
+from typing import Iterable
+# from collections import Iterable   # < py38
+
+
+def flatten(items):
+    """Yield items from any nested iterable; see Reference."""
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            for sub_x in flatten(x):
+                yield sub_x
+        else:
+            yield x
 
 
 def midi_to_samples(file_name, encode_length=False, num_notes=96, samples_per_measure=96):
@@ -28,17 +43,21 @@ def midi_to_samples(file_name, encode_length=False, num_notes=96, samples_per_me
     for track in mid.tracks:
         for msg in track:
             if msg.type == 'time_signature':
-                new_tpm = ticks_per_measure * msg.numerator / msg.denominator  # adapt ticks per measure of this specific song
+                # adapt ticks per measure of this specific song
+                new_tpm = ticks_per_measure * msg.numerator / msg.denominator
 
                 # skip if we find multiple time signatures in the song
                 if has_time_sig and new_tpm != ticks_per_measure:
-                    raise NotImplementedError('Multiple time signatures not supported')
+                    raise NotImplementedError(
+                        'Multiple time signatures not supported')
 
                 ticks_per_measure = new_tpm
                 has_time_sig = True
 
     # turn tracks into pianoroll representation
+    # all_notes format: { pitch# : [start, stop, velocity] }
     all_notes = {}
+    note_velocities = {}
     for track in mid.tracks:
 
         abs_time = 0
@@ -59,7 +78,8 @@ def midi_to_samples(file_name, encode_length=False, num_notes=96, samples_per_me
                 # transform the notes into the 96 heights
                 note = msg.note - (128 - num_notes) / 2
                 if note < 0 or note >= num_notes:  # ignore a note that is outside of that range
-                    print('Ignoring', file_name, 'note is outside 0-%d range' % (num_notes - 1))
+                    print('Ignoring', file_name,
+                          'note is outside 0-%d range' % (num_notes - 1))
                     return []
 
                 # count the number of played notes per pitch
@@ -68,50 +88,71 @@ def midi_to_samples(file_name, encode_length=False, num_notes=96, samples_per_me
                 else:
                     single_note = all_notes[note][-1]
                     if len(single_note) == 1:
-                        single_note.append(single_note[0] + 1)
+                        single_note.append(single_note[0][0] + 1)
 
                 # store the time a note has been played
-                all_notes[note].append([abs_time * samples_per_measure / ticks_per_measure])
+                all_notes[note].append(
+                    [[abs_time * samples_per_measure / ticks_per_measure, float(msg.velocity)]])
 
             # if a note ends
             elif msg.type == 'note_off':
 
-                # if the note has already ended before (note_on, note_off, note_off), we skip the event
+                # if the note has already ended before (note_on, note_off, yeahnote_off), we skip the event
                 if len(all_notes[note][-1]) != 1:
                     continue
+
                 # store the time a note stops playing
-                all_notes[note][-1].append(abs_time * samples_per_measure / ticks_per_measure)
+                all_notes[note][-1].append(abs_time *
+                                           samples_per_measure / ticks_per_measure)
 
     # any note did not end playing, we end it one time tick later
     for note in all_notes:
         for start_end in all_notes[note]:
             if len(start_end) == 1:
-                start_end.append(start_end[0] + 1)
+                start_end.append(start_end[0][0] + 1)
 
-    # put the notes into their respective sample/measure panel (96 x 96)
-    samples = []
+    # flatten each list in elements of all_notes and swap the velocity with the stop time
+    i = 0  # list index
     for note in all_notes:
-        for start, end in all_notes[note]:
-            sample_ix = int(start / samples_per_measure)  # find the sample/measure this belongs into
-            assert (sample_ix < 1024 * 1024)
+        for info_list in all_notes[note]:
 
-            # fill in silence until the appropriate sample/measure is reached
-            while len(samples) <= sample_ix:
-                samples.append(np.zeros((samples_per_measure, num_notes), dtype=np.uint8))
+            # replace the info list of each note with a flattened version of itself
+            all_notes[note][i] = list(flatten(info_list))
 
-            # get sample and find its start to encode the start of the note
-            sample = samples[sample_ix]
-            start_ix = int(start - sample_ix * samples_per_measure)
-            sample[start_ix, int(note)] = 1
+            # swap 2nd and 3rd elements of each flattened list
+            all_notes[note][i][1], all_notes[note][i][2] = all_notes[note][i][2], all_notes[note][i][1]
 
-            # play note until it ends if we encode length
-            if encode_length:
-                end_ix = min(end - sample_ix * samples_per_measure, samples_per_measure)
-                while start_ix < end_ix:
-                    sample[start_ix, int(note)] = 1
-                    start_ix += 1
+            i += 1
 
-    return samples
+        i = 0
+
+    for note in all_notes:
+        print(note, all_notes[note], "\n")
+
+    # # put the notes into their respective sample/measure panel (96 x 96)
+    # samples = []
+    # for note in all_notes:
+    #     for start, end in all_notes[note]:
+    #         sample_ix = int(start / samples_per_measure)  # find the sample/measure this belongs into
+    #         assert (sample_ix < 1024 * 1024)
+
+    #         # fill in silence until the appropriate sample/measure is reached
+    #         while len(samples) <= sample_ix:
+    #             samples.append(np.zeros((samples_per_measure, num_notes), dtype=np.uint8))
+
+    #         # get sample and find its start to encode the start of the note
+    #         sample = samples[sample_ix]
+    #         start_ix = int(start - sample_ix * samples_per_measure)
+    #         sample[start_ix, int(note)] = 1
+
+    #         # play note until it ends if we encode length
+    #         if encode_length:
+    #             end_ix = min(end - sample_ix * samples_per_measure, samples_per_measure)
+    #             while start_ix < end_ix:
+    #                 sample[start_ix, int(note)] = 1
+    #                 start_ix += 1
+
+    # return samples
 
 
 def samples_to_midi(samples, file_name, threshold=0.5, num_notes=96, samples_per_measure=96):
@@ -139,7 +180,8 @@ def samples_to_midi(samples, file_name, threshold=0.5, num_notes=96, samples_per
     piano = 1
     honky_tonk_piano = 4
     xylophone = 14
-    program_message = Message('program_change', program=piano, time=0, channel=0)
+    program_message = Message(
+        'program_change', program=piano, time=0, channel=0)
     track.append(program_message)
 
     abs_time = 0
@@ -152,11 +194,13 @@ def samples_to_midi(samples, file_name, threshold=0.5, num_notes=96, samples_per
 
                 if sample[y, x] >= threshold and (y == 0 or sample[y - 1, x] < threshold):
                     delta_time = abs_time - last_time
-                    track.append(Message('note_on', note=int(note), velocity=127, time=int(delta_time)))
+                    track.append(Message('note_on', note=int(
+                        note), velocity=127, time=int(delta_time)))
                     last_time = abs_time
 
                 if sample[y, x] >= threshold and (y == sample.shape[0] - 1 or sample[y + 1, x] < threshold):
                     delta_time = abs_time - last_time
-                    track.append(Message('note_off', note=int(note), velocity=127, time=int(delta_time)))
+                    track.append(Message('note_off', note=int(
+                        note), velocity=127, time=int(delta_time)))
                     last_time = abs_time
     mid.save(file_name)
